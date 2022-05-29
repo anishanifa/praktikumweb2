f9a5f160dddd1b83e3c9f680b695d7914c715cbd
Anis Hanifa <101486425+anishanifa@users.noreply.github.com>
2022-05-29 18:14:56 +0800

    Update ErrorPage.php

--- PRAK601/ErrorPage.php
+++ PRAK601/ErrorPage.php
@@ -1,4 +1,17 @@
-<?php
-    echo "Anda tidak memiliki akses ke halaman ini!";
-    echo "</br>Silahkan <a href= 'FormLogin.php'>Login</a>";
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Form Login</title>
+    </head>
+    <style>
+            body {
+                background-color: #59936C;
+            }
+        </style>
+    <body>
+    <?php
+        echo "Anda tidak memiliki akses ke halaman ini!";
+        echo "</br>Silahkan <a href= 'FormLogin.php'>Login</a> untuk mengakses halaman";
+    ?>
+    </body>
+</html>
